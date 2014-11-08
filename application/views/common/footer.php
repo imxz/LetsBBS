e69fdabb4b6fd818ff17bdd2ca8a5f76446aa000
Ethan <mr.ethanzhou@gmail.com>
2014-11-08 13:55:01 +0800

    update footer

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -7,4 +7,5 @@
         <script src="<?php echo base_url('static/js/jquery.min.js');?>"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="<?php echo base_url('static/js/bootstrap.min.js');?>"></script>
+        <?php echo $site_analysis;?>
     </div><!-- /.footer -->
