7f142f62a184cf49bd171d6e938f64da5349ef9b
Ethan <mr.ethanzhou@gmail.com>
2014-12-11 14:49:03 +0800

    add update controller file

---
+++ application/controllers/update.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Update extends Front_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('welcome_message');
+    }
+}
+
+/* End of file update.php */
+/* Location: ./application/controllers/update.php */
